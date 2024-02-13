fece287f4597e45152fc22816f068c56de20749a
Tess-mltx <maloteaux.stacy@gmail.com>
2024-02-13 12:50:05 +0100

    script.js sign-up.js

--- index.php
+++ index.php
@@ -6,31 +6,32 @@ require('connect.php');
 <head>
 	<meta charset="utf-8">
 	<title>Hacker Poulette - formulaire</title>
-	<link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="style/style.scss" />
+    <script type="module" defer src="js/script.js"></script>
 </head>
 <body>
 	<h1>Create profile</h1>
 	<form action="create_user.php" method="post">
 		<div>
 			<label for="fistname">Firstname</label>
-			<input type="text" name="firstname" value="" required minlength="2" maxlength="255">
+			<input type="text" name="firstname" id="firstname" value="" required minlength="2" maxlength="255">
 		</div>
 		<div>
 			<label for="lastname">Lastname</label>
-			<input type="text" name="lastname" value="" required minlength="2" maxlength="255">
+			<input type="text" name="lastname" id="lastname" value="" required minlength="2" maxlength="255">
 		</div>
             <label for="email">E-mail adress</label>
-			<input type="email" name="email" value="" required minlength="2" maxlength="255">
+			<input type="email" name="email" id="email" value="" required minlength="2" maxlength="255">
 		
 		<div>
 			<label for="file">Picture's profile</label>
-            <input type="file" name="file" accept="image/png, image/jpeg, .gif" required/>		</div>
+            <input type="file" name="file" id="file" accept="image/png, image/jpeg, .gif" required/>		</div>
 
 		<div>
 			<label for="description">Description</label>
-			<input type="textarea" name="description" value="" required minlength="2" maxlength="1000">
+			<input type="textarea" name="description" id="description" value="" required minlength="2" maxlength="1000">
 		</div>
-		<button type="submit" name="button">Envoyer</button>
+		<button type="submit" name="button" id="button">Envoyer</button>
 	</form>
 </body>
 </html>
---
+++ js/modules/sign-up_form.js
@@ -0,0 +1,15 @@
+const submit = document.querySelector('#button')
+
+submit.addEventListener('click', () => {
+    //  worck-flow event
+});
+
+function verificationDataForm() {
+    //  function to verify form
+    const firstname = document.getElementById('firstname');
+    const lastname = document.getElementById('lastname');
+    const email = document.getElementById('email');
+    const avatar = document.getElementById('avatar');
+    const description = document.getElementById('description');
+};
+
