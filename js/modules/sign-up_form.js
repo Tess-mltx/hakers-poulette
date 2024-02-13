b68102a47bf382689eedf3e4deb2d29346e0b41e
Tess-mltx <maloteaux.stacy@gmail.com>
2024-02-13 14:11:37 +0100

    verification text

--- js/modules/sign-up_form.js
+++ js/modules/sign-up_form.js
@@ -6,10 +6,29 @@ submit.addEventListener('click', () => {
 
 function verificationDataForm() {
     //  function to verify form
-    const firstname = document.getElementById('firstname');
-    const lastname = document.getElementById('lastname');
-    const email = document.getElementById('email');
+    const firstname = document.getElementById('firstname').value;
+    const lastname = document.getElementById('lastname').value;
+    const email = document.getElementById('email').value;
     const avatar = document.getElementById('avatar');
-    const description = document.getElementById('description');
+    const description = document.getElementById('description').value;
+
+    verificationText(firstname, 2, 255);
+    verificationText(lastname, 2, 255);
+    verificationText(description, 2, 1000);
+
 };
+
+function verificationText(input, min, max) {
+    if (input < min || input > max) {
+        // inssert text in form
+    }
+};
+
+function verificationEmail() {
+    
+};
+
+function verificationAvatar() {
+    if (avatar )
+}
 
