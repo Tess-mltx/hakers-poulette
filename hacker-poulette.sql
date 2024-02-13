81f54738d9ab027d9c1f4b240ed63aa75068cf48
Tess-mltx <maloteaux.stacy@gmail.com>
2024-02-13 14:14:13 +0100

    add correct db

--- hacker-poulette.sql
+++ hacker-poulette.sql
@@ -1,6 +1,21 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 13 fév. 2024 à 13:13
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.26
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données : `hacker-poulette`
@@ -15,12 +30,23 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `firstname` varchar(255) NOT NULL,
+  `firstname` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `email` varchar(255) NOT NULL,
   `file_path` varchar(255) DEFAULT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `file_path`, `description`) VALUES
+(1, 'Tess', 'maloteaux', 'maloteaux.stacy@gmail.com', '', 'Je suis un renard qui vis dans un monde imaginaire; parcourant les plaines; écrivant des histoire de 1000 charactères pour remplir ce textarea !'),
+(2, 'stacy', 'la vanne', 'bienvenue@inkde.be', '', 'dans un monde imaginaire; parcourant les plaines; écrivant des histoire de 1000 charactères pour remplir ce textarea !');
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
