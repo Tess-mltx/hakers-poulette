88b362036b3a466da7aa1a95bfad966186e3d287
marvinseran <marvinseran@gmail.com>
2024-02-16 14:54:46 +0100

    pushcaptcha

--- create_user.php
+++ create_user.php
@@ -5,55 +5,17 @@ if (isset($_POST['button'])) {
         $firstname = isset($_POST['firstname']) ? $_POST['firstname'] : '';
         $lastname = isset($_POST['lastname']) ? $_POST['lastname'] : '';
         $email = isset($_POST['email']) ? $_POST['email'] : '';
-        $avatar = isset($_FILES['file']) ? $_FILES['file'] : '';
+        $avatar = isset($_POST['avatar']) ? $_POST['avatar'] : '';
         $description = isset($_POST['description']) ? $_POST['description'] : '';        
 
-        $target_dir = "assets/avatars_users/";
-        $target_file = $target_dir . basename($_FILES['file']['name']);
-        $uploadOk = 1; // ca c'est un score pour vérifier que l'img est ok
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
         if (!empty($firstname) && !empty($lastname) && !empty($email) && !empty($description)){
-            
-            if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // J'ajoute la validation mail cross figer !
-                echo "Email address '$email' is considered valid.\n";
-                if (!empty($avatar['name']) && $avatar['error'] === UPLOAD_ERR_OK) { // completer la condition si le file a load dans le form
-                    // Vérifier si le fichier est une image
-                    $check = getimagesize($avatar["tmp_name"]); // si la methode fonctionne c'est que c'est une img
-                    if($check !== false) {
-                        echo "Le fichier est une image - " . $check["mime"] . "."; // type MIME du média
-                        $uploadOk = 1;
-                    } else {
-                        echo "Le fichier n'est pas une image.";
-                        $uploadOk = 0;
-                    }  
-                    // Si tout est ok déplacer le fichier vers le dossier
-                    if ($uploadOk == 1 && move_uploaded_file($avatar["tmp_name"], $target_file)) {
-
-                        $query = $bdd->prepare('INSERT INTO users (firstname, lastname, email, file_path, description) VALUES (:firstname, :lastname, :email, :avatar, :description)');
-                        $query->execute(array(':firstname' => $firstname, ':lastname' => $lastname, ':email' => $email, ':avatar' => $target_file, ':description' => $description));
-
-                        echo "Le fichier ". htmlspecialchars(basename($avatar["name"])) . " a été téléchargé et les informations ont été enregistrées dans la base de données.";
-                    } else {
-                        echo "Désolé, une erreur s'est produite lors du téléchargement de votre fichier.";
-                    }
-                } else {
-            
-                $query = $bdd->prepare('INSERT INTO users (firstname, lastname, email, description) VALUES (:firstname, :lastname, :email, :description)');
-                $query->execute(array(':firstname' => $firstname, ':lastname' => $lastname, ':email' => $email, ':description' => $description));
-
-                echo "Les informations ont été enregistrées dans la base de données.";
-                }
-
-                header("Location: index.php");
-                exit();
-        
-            } else {
-                echo "l'adresse mail '$email' est invalide.\n";
-            }
-
-    } else {
-        echo "Veuillez remplir tous les champs du formulaire.";
-    }
+            $query = $bdd->prepare('INSERT INTO users (firstname, lastname, email, file_path, description) VALUES (:firstname, :lastname, :email, :avatar, :description)');
+            $query->execute(array(':firstname' => $firstname, ':lastname' => $lastname, ':email' => $email, ':avatar' => $avatar, ':description' => $description));
+            // add msg to confirm sending form ?
+            // header("Location: ");
+            exit();
+        } else {
+            echo "Veuillez remplir tous les champs du formulaire.";
+        }
 } 
 ?>
--- index.php
+++ index.php
@@ -2,44 +2,80 @@
 require('connect.php');
 ?>
 <!DOCTYPE html>
-<html>
+<div class="w-full h-full flex flex-col items-center justify-center text-aliceblue">
+	<html class="custom-background">
 
-<head>
-	<meta charset="utf-8">
-	<title>Hacker Poulette - formulaire</title>
-	<link rel="stylesheet" href="style/style.scss" />
-	<script type="module" defer src="js/script.js"></script>
-</head>
+	<head>
+		<meta charset="utf-8">
+		<title>Hacker Poulette - formulaire</title>
 
-<body>
-	<div class="form-container">
+		<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+		<link href="src/output.css" rel="stylesheet">
+		<script type="module" defer src="js/script.js"></script>
+	</head>
 
-		<form class="form__body" action="create_user.php" method="post">
-			<h1 class="form__body__title">Create profile.</h1>
-			<div class="form__body__content">
-				<input placeholder="Firstname"  type="text" name="firstname" id="firstname" value="" required minlength="2" maxlength="255">
-			</div>
-			<div class="form__body__content">
-				<input placeholder="Lastname" type="text" name="lastname" id="lastname" value="" required minlength="2" maxlength="255">
-			</div>
-			<div class="form__body__content">
-				<input placeholder="E-mail adress" type="email" name="email" id="email" value="" required minlength="2" maxlength="255">
-			</div>
-			<div class="form__body__content">
-				<input placeholder="Description" type="textarea" name="description" id="description" value="" required minlength="2" maxlength="1000">
-			</div>
-			<div class="form__body__content">
-				<label class="form__body__content__file">
-					
-				<input type="file" name="file" id="file" accept="image/png, image/jpeg, .gif" required />
-				Add Avatar<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h9v2H5v14h14v-9h2v9q0 .825-.587 1.413T19 21zM17 9V7h-2V5h2V3h2v2h2v2h-2v2zM6 17h12l-3.75-5l-3 4L9 13zM5 5v14z"/></svg>
-				</label>
-			</div>
-			<button class="button__submit" type="submit" name="button" id="button">Send</button>
-			<h2>TEST</h2>
-		</form>
-	</div>
+	<header>
+		<div class="flex items-center justify-end bg-[#171717] rounded-2xl m-8 pt-4 pb-4 pl-8 pr-8 text-aliceblue">
+			<input class="mr-4 bg-transparent border-none outline-none w-full text-aliceblue flex items-center justify-center gap-2 rounded-full p-2 bg-[#171717] shadow-[inset_2px_5px_10px_rgb(5,5,5)]" placeholder="Admin Password" type="adminpass" name="adminpass" id="adminpass" value="" required minlength="2" maxlength="255">
+			<button class="w-3/5 my-4 py-2 rounded bg-[#252525] text-white transition duration-400 ease-in-out hover:bg-green-500" type="submit" name="button" id="button">Connect</button>
+		</div>
+	</header>
 
-</body>
+	<body class="flex flex-col items-center justify-center m-8">
+	
 
-</html>
+	<?php
+		require_once 'autoload.php';
+			if(isset($_POST['button'])){
+				$recaptcha = new \ReCaptcha\ReCaptcha("6LdzQ3UpAAAAAHGi9cxEJEPcBsh5-vvwlkfaaoyr");
+				$gRecaptchaResponse = $_POST['g-recaptcha-response'];
+				$resp = $recaptcha->setExpectedHostname('recaptcha-demo.appspot.com')
+								  ->verify($gRecaptchaResponse, $remoteIp);
+				if ($resp->isSuccess()) {
+					// Verified!
+					echo "Success !";
+				} else {
+					$errors = $resp->getErrorCodes();
+					var_dump($errors);
+				}
+			}
+		?>
+		<div class="w-full h-full flex items-center justify-center text-[aliceblue] m-0;">
+
+			<form action="create_user.php" method="post">
+				<div class="flex flex-col items-center justify-center bg-[#171717] rounded-2xl m-8 p-8 text-aliceblue">
+					<h1 class="text-4xl mb-4">Create profile.</h1>
+					<div class="form__body__content">
+						<input class="mb-4 bg-transparent border-none outline-none w-full text-d3d3d3 flex self-center justify-center gap-2 rounded-full p-2 bg-[#171717] shadow-[inset_2px_5px_10px_rgb(5,5,5)]" placeholder="Firstname" type="text" name="firstname" id="firstname" value="" required minlength="2" maxlength="255">
+					</div>
+					<div class="form__body__content">
+						<input class="mb-4 bg-transparent border-none outline-none w-full text-d3d3d3 flex items-center justify-center gap-2 rounded-full p-2 bg-[#171717] shadow-[inset_2px_5px_10px_rgb(5,5,5)]" placeholder="Lastname" type="text" name="lastname" id="lastname" value="" required minlength="2" maxlength="255">
+					</div>
+					<div class="form__body__content">
+						<input class="mb-4 bg-transparent border-none outline-none w-full text-d3d3d3 flex items-center justify-center gap-2 rounded-full p-2 bg-[#171717] shadow-[inset_2px_5px_10px_rgb(5,5,5)]" placeholder="E-mail adress" type="email" name="email" id="email" value="" required minlength="2" maxlength="255">
+					</div>
+					<div class="form__body__content">
+						<input class="mb-4 bg-transparent border-none outline-none w-full text-d3d3d3 flex items-center justify-center gap-2 rounded-full p-2 bg-[#171717] shadow-[inset_2px_5px_10px_rgb(5,5,5)]" placeholder="Description" type="textarea" name="description" id="description" value="" required minlength="2" maxlength="1000">
+					</div>
+					<div class="form__body__content">
+						<label class="flex items-center justify-center gap-2 text-[#d3d3d3] rounded-full p-2 bg-[#171717] shadow-[inset_2px_5px_10px_rgb(5,5,5)] cursor-pointer">
+							<input class="hidden" type="file" name="file" id="file" accept="image/png, image/jpeg, .gif" required />
+							Add Avatar
+							<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+								<path fill="currentColor" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h9v2H5v14h14v-9h2v9q0 .825-.587 1.413T19 21zM17 9V7h-2V5h2V3h2v2h2v2h-2v2zM6 17h12l-3.75-5l-3 4L9 13zM5 5v14z" />
+							</svg>
+						</label>
+					</div>
+					<form action="create_user.php" method="POST" class="mt-4">
+						<div class="g-recaptcha" data-sitekey="6LdzQ3UpAAAAAHck6Y60GCABXhwLamGRCb91xhNP"></div>
+						<br />
+						<button class="w-3/5 my-4 py-2 rounded bg-[#252525] text-white transition duration-400 ease-in-out hover:bg-green-500" type="submit" name="button" id="button">Send</button>
+					</form>
+				</div>
+			</form>
+		</div>
+
+	</body>
+
+	</html>
+</div>
